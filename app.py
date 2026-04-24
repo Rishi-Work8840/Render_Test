3a9a8c265e65c2f5ccea0092190a0269c88f12ee
Rishi Dave <rishidavework@gmail.com>
2026-04-24 10:15:05 +0530

    initial deploy - flask app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Backend is running!"})
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
